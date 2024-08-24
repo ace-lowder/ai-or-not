041bdf1efe2a369af095cedb5cbe9abd0b5f09cf
Ace <ace.lowder@gmail.com>
2024-08-24 15:19:25 -0700

    Center floating robot head with dialogue box

--- src/components/Dialogue.tsx
+++ src/components/Dialogue.tsx
@@ -24,11 +24,11 @@ const Dialogue: React.FC<DialogueProps> = ({ children }) => {
   }, [words]);
 
   return (
-    <div className="w-full">
-      <div className="float relative">
-        <span className="text-6xl sway absolute">🤖</span>
+    <div className="flex gap-[5.5rem] items-center">
+      <div className="float my-auto text-sm text-gray-700 inset-0">
+        `<span className="-top-6 text-6xl sway absolute">🤖</span>
       </div>
-      <div className="ml-[5.5rem] flex-grow bg-white outline outline-[6px] outline-gray-700 text-gray-700 font-bold text-center rounded-xl p-4 shadow-lg relative">
+      <div className="bg-white outline outline-[6px] outline-gray-700 text-gray-700 font-bold text-center rounded-xl p-4 shadow-lg relative">
         <span className="text-white">{children}</span>
         <span className="absolute inset-0 p-4 box-content">
           {visibleWords.join(' ')}
